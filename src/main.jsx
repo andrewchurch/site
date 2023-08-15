d9b1b2ee0c59f635edbd91639645567971a37363
Andrew Church <andrewchurch@gmail.com>
2023-08-15 17:57:34 -0400

    Adjusting layout to allow for navigation

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,5 @@ import Arcade from './Arcade.jsx'
 import Portfolio from './Portfolio.jsx'
 import './index.css'
 
-//ReactDOM.createRoot(document.getElementById('arcade')).render(<Arcade />)
+ReactDOM.createRoot(document.getElementById('arcade')).render(<Arcade />)
 ReactDOM.createRoot(document.getElementById('portfolio')).render(<Portfolio />)
