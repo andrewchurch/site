0543365bd1067ba6b8c779c031f372bc22f3a263
Andrew Church <andrewchurch@gmail.com>
2023-08-16 17:14:43 -0400

    Added custom arcade font

--- src/Arcade.jsx
+++ src/Arcade.jsx
@@ -8,7 +8,7 @@ function Player({ style }) {
             className="w-6 h-6 inline-block -rotate-90 transition-transform origin-center duration-500"
             style={style}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
-        </svg>  
+        </svg>
     )
 }
 
@@ -32,7 +32,7 @@ export default function Arcade() {
             <Player
                 style={playerStyle}
             />
-            <p className="mt-2 text-xs">Coming Soon</p>
+            <p className="mt-2 text-xl tracking-wide font-arcade">Coming Soon</p>
         </div>
     )
 }
