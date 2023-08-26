6e7fc2d259db9154190fed7398bd7bd988b0b2db
Andrew Church <andrewchurch@gmail.com>
2023-08-26 15:36:35 -0400

    Updating start language

--- src/games/FlappyDude/Start.jsx
+++ src/games/FlappyDude/Start.jsx
@@ -7,7 +7,7 @@ export default function StartScreen({onClick}) {
             <div className="m-auto text-center">
                 <h2 className="mb-2 text-2xl tracking-wide font-arcade">Flappy Dude</h2>
                 <Player />
-                <p className="mt-6 text-base tracking-wide font-arcade">Click to Start</p>
+                <p className="mt-6 text-base tracking-wide font-arcade">Click/Tap to Start</p>
             </div>
         </div>
     )
